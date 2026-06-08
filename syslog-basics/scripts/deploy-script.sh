380ca057b0dd0f64b90cf553b70d268cce67d58b
Greg <44036625+akademsubotnik@users.noreply.github.com>
2026-06-08 17:50:19 +0000

    update project plan

---
+++ syslog-basics/scripts/deploy-script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "========================================="
+echo "Deploying up NETCONF RESTCONF Basics Lab"
+echo "========================================="
+echo ""
+
+# Destroy containerlab topology
+#sudo containerlab destroy -t topology.clab.yml
+
+echo ""
+echo "Lab deployed successfully!"
+echo ""
